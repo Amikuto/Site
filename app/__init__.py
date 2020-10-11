b7b7ec71a3160d0bd2ab5647e6bb8fbb9d6bcb15
Amikuto <Amikuto@users.noreply.github.com>
2020-10-11 23:59:34 +0300

    flask-admin added

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_admin import Admin
 
 
 app = Flask(__name__)
